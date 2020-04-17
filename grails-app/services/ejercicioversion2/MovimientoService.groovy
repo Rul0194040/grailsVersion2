6b4e7a8237135d97faeb3092f6fe93b43b7ce68c
Raul de Jesus Sierra Vasconcelos <rsierravasconcelos@gmail.com>
2020-04-16 19:02:36 -0500

    ultimo intento3

--- grails-app/services/ejercicioversion2/MovimientoService.groovy
+++ grails-app/services/ejercicioversion2/MovimientoService.groovy
@@ -31,48 +31,61 @@ class MovimientoService {
     }
 
     Movimiento build(Movimiento movinientoInstance, Map json) {
-        movinientoInstance.descuento = json.descuento as double
-        movinientoInstance.total = 0
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy") // 01-01-2020
-        movinientoInstance.fechaVendido = sdf.parse(json.fechaVendido as String)
-        List<Detalle> detalles = Arrays.asList(json.detalles) as List<Detalle>
+
+        if(movinientoInstance.id == null){
+            movinientoInstance = new Movimiento()
+            movinientoInstance.total = 0
+            movinientoInstance.fechaVendido = new Date()
 
 
-        if (movinientoInstance.id != null){
-            for (int i = 0; i < detalles.size(); i++) {
-                Detalle detalle = detalles.get(i)
-                detalle.cantidad = detalles[i].cantidad
-                if (detalles[i]?.articulo instanceof Integer) {
-                    detalle.articulo = articuloService.get(detalles[i]?.articulo as Long)
-                } else {
-                    detalle.articulo = articuloService.get(detalles[i].articulo.id as long)
-                }
-                if (detalle.articulo != null) {
-                    detalle.precio = detalle.articulo.precio
-                    double descuento = ((detalle.cantidad * detalle.precio)*movinientoInstance.descuento)
-                    movinientoInstance.total += (detalle.cantidad * detalle.precio) - descuento
-                    detalle.movimiento = movinientoInstance
-                }
-            }
-        }else{
+
+            List detalles = json.detalles
             for (int i = 0; i < detalles.size(); i++) {
                 Detalle detalle = new Detalle()
                 detalle.cantidad = detalles[i].cantidad
                 if (detalles[i]?.articulo instanceof Integer) {
-                    detalle.articulo = articuloService.get(detalles[i]?.articulo as Long)
+                    detalle.articulo = articuloService.get(detalles[i]?.articulo)
                 } else {
                     detalle.articulo = articuloService.get(detalles[i].articulo.id as long)
                 }
                 if (detalle.articulo != null) {
                     detalle.precio = detalle.articulo.precio
-                    double descuento = ((detalle.cantidad * detalle.precio)*movinientoInstance.descuento)
-                    movinientoInstance.total += (detalle.cantidad * detalle.precio) - descuento
+                    movinientoInstance.total += (detalle.cantidad * detalle.precio)*((detalle.cantidad * detalle.precio)*movinientoInstance.descuento)
                     detalle.movimiento = movinientoInstance
+                    movinientoInstance.addToDetalles(detalle)
+                }
+            }
+
+
+        }else{
+            movinientoInstance.total = 0
+
+            List detalles = json.detalles
+            println "detalles: $detalles"
+            for (int i = 0; i < detalles.size(); i++) {
+                Detalle detalle
+                if (detalles[i].id)
+                    detalle = Detalle.get(detalles[i].id)
+                else
+                    detalle = new Detalle()
+
+                detalle.cantidad = detalles[i].cantidad
+                if (detalles[i]?.articulo instanceof Integer) {
+                    detalle.articulo = Articulo.get(detalles[i].articulo as long)
+                } else {
+                    detalle.articulo = Articulo.get(detalles[i].articulo.id as long)
+                }
+                if (detalle.articulo != null) {
+                    detalle.precio = detalle.articulo.precio
+                    movinientoInstance.total += (detalle.cantidad * detalle.precio)*((detalle.cantidad * detalle.precio)*movinientoInstance.descuento)
+
+                    if (detalles[i].id == null) {
+                        detalle.movimiento = movinientoInstance
+                        movinientoInstance.addToDetalles(detalle)
+                    }
                 }
             }
         }
-
-
 
         return movinientoInstance
     }
